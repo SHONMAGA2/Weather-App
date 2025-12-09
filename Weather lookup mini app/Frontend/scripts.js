0e593cadd7287f580ca574105041b14fd48885e2
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-12-09 13:05:39 +0200

    saving changes  p2

--- Weather lookup mini app/Frontend/scripts.js
+++ Weather lookup mini app/Frontend/scripts.js
@@ -5,4 +5,23 @@ async function fetchWeather() {
     if(!city_name) return;
 
     const url = "http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric";
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if(data.cod !== 200){
+            document.getElementById("weather-info").innerHTML = "City not found";
+            return;
+        }
+
+        document.getElementById(weather-info).innerHTML = `
+        <h2>${data.name}</h2>
+        <p> Temperature: ${data.main.temp}C</p>
+        <p>Condition: ${data.weather[0].description}</p>
+        `;
+
+    } catch (error) {
+        document.getElementById("weather-info").innerHTML = "Network Issue";      
+    }
 }
